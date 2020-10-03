1e156b01513f0c94a933826ad39c3ddcf69ae8e3
Jamilly Pinheiro Lima <jamillyp@alu.ufc.br>
2020-10-03 15:08:08 -0300

    update: Paginas

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const { request } = require('http');
 const bodyParser = require('body-parser');
+const path = require('path')
 
 // config porta do servidor
 const portaHttp = 9999;
@@ -21,10 +22,19 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+// public
+app.use(express.static(path.join(__dirname, "public")))
+
 // rota get p renderizar a home
 app.get('/', (request,response) => {
     response.render('home.handlebars')
 });
+// rota post p barra de pequisa da home
+app.post('/', (request,response) => {
+    let nomeEvento = request.body.nomeEvento;
+    let dataEvento = request.body.dataEvento;
+    console.log(nomeEvento + ' -- ' + dataEvento);
+})
 
 // rota para cadastro usando o render p renderizar a pagina criada (carregar pagina)
 app.get('/cadastrar', (request,response) => {
@@ -44,12 +54,35 @@ app.post('/cadastrar', (request,response) => {
 app.get('/login', (request,response) => {
     response.render('login.handlebars')
 });
-
 // rota post p login 
 app.post('/login', (request, response) => {
-    let email = request.body.nome;
+    let email = request.body.email;
     let senha = request.body.senha;
     console.log(email + ' -- ' + senha);
+})
+
+// rota get para cadastrar evento
+app.get('/addEvento', (request,response) => {
+    response.render('cadastroEvento.handlebars')
+});
+// rota post para cadastrar evento
+app.post('/addEvento', (request, response) => {
+    let nomeEvento = request.body.nomeEvento;
+    let local = request.body.local;
+    let dataEvento = request.body.dataEvento;
+    let horario = request.body.horario;
+    console.log(nomeEvento + ' -- ' + local + ' -- ' + dataEvento + ' -- ' + horario);
+})
+
+// rota get para buscar evento
+app.get('/buscarEvento', (request,response) => {
+    response.render('buscarEvento.handlebars')
+});
+// rota post para buscar evento
+app.post('/buscarEvento', (request, response) => {
+    let nomeEvento = request.body.nomeEvento;
+    let dataEvento = request.body.dataEvento;
+    console.log(nomeEvento + ' -- ' + dataEvento);
 })
 
 // servidor
